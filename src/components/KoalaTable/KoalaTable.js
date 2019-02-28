b8110896b89aad283140843ac0f6af8d4600ea3b
Peng Xue Vang <pxvang33@gmail.com>
2019-02-28 15:01:33 -0600

    added get koala saga and function

--- src/components/KoalaTable/KoalaTable.js
+++ src/components/KoalaTable/KoalaTable.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class KoalaTable extends Component {
+    componentDidMount() {
+        this.getKoalas();
+    }
 
+    getKoalas = () => {
+        this.props.dispatch({type: 'GET_KOALAS'})
+    }
     render() {
         return (
             <div>
                 <h2>Koala Table</h2>
+                {JSON.stringify(this.props)}
             </div>
         )
     }
